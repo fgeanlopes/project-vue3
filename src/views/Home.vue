141719b7cefb6ffdb332e49b234be2d867d31e49
devel_37 <devel_37@autoavaliar.com.br>
2023-05-24 09:08:39 -0300

    initial

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <Banner/>
+  <div class="main-container">
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+
+import Banner from '../components/Banner.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Banner,
+  },
+};
+
+</script>
+
+<style scoped>
+</style>
